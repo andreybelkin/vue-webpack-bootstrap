d227835b4fa556e799a8f46dd5332603eb2d95d6
Andrey Belkin <abelkin@abelkin.com>
2018-11-21 01:13:28 +0500

    Add tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,20 @@
 module.exports = {
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    'plugin:vue/recommended',
-    'standard'
-  ],
-  plugins: [
-    'vue'
-  ]
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        'plugin:vue/recommended',
+        'standard'
+    ],
+    plugins: [
+        'vue'
+    ],
+    env: {
+        browser: true,
+        node: true,
+        mocha: true
+    },
+    globals: {
+        expect: true
+    }
 }
