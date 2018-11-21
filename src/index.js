1d2973974b5aacc76fc9a0cde3000608669fa924
Andrey Belkin <abelkin@abelkin.com>
2018-11-22 03:01:54 +0500

    Add v-tooltip module

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import App from './App.vue'
 import './assets/css/app.css'
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+import VTooltip from 'v-tooltip'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+Vue.use(VTooltip)
 
 /* eslint-disable-next-line no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
 })
-
