ad046e2f2e233626d3c0c8be081ddbcac854fc3a
Andrey Belkin <abelkin@abelkin.com>
2018-11-22 02:52:32 +0500

    Add css import to app entry point Add bootstrap module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/app.css'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.use(BootstrapVue)
 
 /* eslint-disable-next-line no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
